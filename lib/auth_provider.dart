e2ac913b8075f81ce81e72308e7036f18fb8fb3b
colin-schulte <colin.schulte01@student.csulb.edu>
2026-06-28 11:54:23 -0700

    fix: remove import statement

--- lib/auth_provider.dart
+++ lib/auth_provider.dart
@@ -4,7 +4,6 @@
 
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:google_sign_in/google_sign_in.dart';
 
 class AuthProvider extends ChangeNotifier {
   final FirebaseAuth _auth = FirebaseAuth.instance;
